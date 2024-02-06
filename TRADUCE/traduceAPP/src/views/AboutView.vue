53e4dba6fbda9d7bae03fc3bb739219ae8b44aa3
canavamesof <caromoramanager@gmail.com>
2024-02-05 18:05:06 -0600

    componente revisar
    escribi parte del componente q el profe te explico, pero creo q es lo mismo q hiciste en googletraductor.vue

--- TRADUCE/traduceAPP/src/views/AboutView.vue
+++ TRADUCE/traduceAPP/src/views/AboutView.vue
@@ -1,7 +1,87 @@
 <template>
-  <p>hola desde about</p>
+  <div style="display: flex; flex-direction: column">
+    <div>
+      <div>Languege source: {{ source }}</div>
+      <select v-model="source">
+        <option v-for="mylanguages in languages">
+          {{ mylanguages.language }}
+        </option>
+      </select>
+    </div>
+    <div>
+      <div>Languege target: {{ target }}</div>
+      <select v-model="target">
+        <option v-for=" mylanguages  in  languages ">
+          {{ mylanguages.language }}
+        </option>
+      </select>
+    </div>
+    <div>
+      <span>Text original:</span>
+      <textarea @change="detectlanguages()
+        " v-model="text" placeholder="add multiple lines">
+    </textarea>
+      <button @click="mountLanguageDetec">
+        {{ myDetectionLanguage === "" ? "Deteccion" : myDetectionLanguage }}
+      </button>
+    </div>
+    <div>
+      <span> Text traslate:</span>
+      <textarea>
+    v-model="textTranslate"
+    placeholder="add multiple lines"
+  </textarea>
+    </div>
+
+    <button @click="translate"> Traducir texto</button>
+  </div>
 </template>
 
-<style>
+<script setup>
+import { onMounted, ref } from "vue";
+import googleTraslateServices from "../services/googleTraslateServices";
+const languages = ref([]);
+const text = ref(***);
+const textTraslate = ref(**);
+const target = ref(**);
+const source = ref(***);
+const myDetectionLanguage = ref("");
 
-</style>
+const mountLanguageDetec = () => {
+  source.value = myDetectionLanguage.value;
+}
+
+const getLanguages = async => {
+const response = await googleTraslateServices.getlanguages();
+  languages.value = response.data.data.languages;
+};
+
+const detectLanguages = async() = => {
+const response = await googleTraslateServices.detectLanguages(text.value);
+
+let detection = response.data.data.detections[0];
+myDetectionLanguage.value = detection [0].language;
+
+translate ();
+}
+const translate = async () => {
+if (text.value !== "" && target.value !== "" && source.value != "")
+
+  const response = await googleTraslateServices.traslate(
+  text.value, 
+  target.value,
+  source.value
+  );
+let translation = response.data.da.translation;
+
+console.log(translation[0].trnaslationText);
+textTraslate.value = translation[0].traslateText;
+};
+
+onMounted(() => {
+    //para hacer pruebas quitar comentario 
+   obtenerIdiomas();    
+  });
+</script>
+
+<style></style>
