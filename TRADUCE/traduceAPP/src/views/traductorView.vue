60e935130386e847442f01c2b4b8054b77a491db
Calithorn <141888310+Calithorn@users.noreply.github.com>
2024-02-18 18:55:01 -0600

    complete

--- TRADUCE/traduceAPP/src/views/traductorView.vue
+++ TRADUCE/traduceAPP/src/views/traductorView.vue
@@ -21,25 +21,23 @@
           </select>
         </div>
       </div>
+
+
       <div class="d-flex justify-content-center">
-        <textarea v-model="text" @change="detectLanguages()" placeholder="Traducir">
-            </v-model></textarea>
+        <textarea @change="detectLanguages()" v-model="text" placeholder="Traducir"></textarea>
+        <button @click="mountLanguageDetect()">{{ myDetectionLanguage === "" ? "Detectar" : myDetectionLanguage }}</button>
+        
       </div>
       <div class="d-flex justify-content-center">
-        <textarea v-model="TextTranslate" placeholder="Texto traducido">
-            </v-model></textarea>
+        <textarea v-model="TextTranslate" placeholder="Texto traducido"></textarea>
+         <button @click="translate">traducir</button>
       </div>
-      <div class="d-flex justify-content-center">
-        <button @change="">
-          {{ myDetectarLenguage === "" ? "Detectar" : myDetectarLenguage }}
-        </button>
-      </div>
-      <div class="d-flex justify-content-center">
-        <button @click="traducirIdiomas">traducir</button>
-      </div>
+     
+      
     </div>
   </div>
 </template>
+
 <script setup>
 import { onMounted, ref } from "vue";
 import googleServices from "@/services/googleServices";
@@ -49,12 +47,42 @@ const target = ref("");
 const languages = ref([]);
 const text = ref("");
 const TextTranslate = ref("");
-const miDetectarLenguage = ref("");
+const myDetectionLanguage = ref("");
 
+const mountLanguageDetect = () =>{
+  source.value = myDetectionLanguage.value;
+};
 
+const getLanguages = async () =>{
+  const response = await googleServices.getLanguages();
+  languages.value = response.data.data.languages;
+};
+
+const detectLanguages = async () =>{
+  const response = await googleServices.detectLanguages(text.value);
+
+  let detection = response.data.data.detection[0];
+  myDetectionLanguage.value = detection[0].language;
+
+  translate();
+
+}
+
+const translate = async () =>{
+  if (text.value !== "" && target.value !== "" && source.value !== "") {
+    const response = await googleServices.translate(
+      text.value,
+      target.value,
+      source.value
+    );
+    let translation = response.data.data.translations;
+    console.log(translation[0].translatedText);
+    TextTranslate.value = translation[0].translatedText;
+  }
+}
 
 
 onMounted(() => {
-  //obtenerIdiomas();
+ getLanguages();
 });
 </script>
